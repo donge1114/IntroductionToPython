955c41b06fd959bbe98d059130a3321f015d1083
donge1114 <donge@rose-hulman.edu>
2017-11-27 15:21:21 -0500

    DONE

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Enyi Dong.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,25 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+jia = rg.SimpleTurtle('turtle')
+jia.pen = rg.Pen('green', 3)
+jia.speed = 10
+jia.left(90)
+jia.forward(50)
+
+size = 250
+for k in range (15):
+    jia.draw_circle(size)
+    jia.pen_up()
+    jia.right(45)
+    jia.forward(10)
+    jia.left(45)
+    jia.pen_down()
+    size = size-15
+
+
+window.close_on_mouse_click()
